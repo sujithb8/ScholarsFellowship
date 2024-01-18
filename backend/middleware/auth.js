6c65f0df875dba72599657bfb4e841ffd8453709
Battu Sujith <72318405+sujithb8@users.noreply.github.com>
2024-01-19 00:15:11 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -12,7 +12,7 @@ exports.isAuthenticated = async (req,res,next) => {
 
     try {
         //verify token
-        const decoded = jwt.verify(token,process.ENV.JWT_TOKEN);
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
         req.user = await User.findById(decoded.id);
         next();
 
